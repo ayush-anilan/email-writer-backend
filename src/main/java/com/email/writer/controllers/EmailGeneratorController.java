f70ae82c11e7b65a9e7e957b447183a9ebea8a32
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2025-02-18 22:44:16 +0530

    minor changes while building project

--- src/main/java/com/email/writer/controllers/EmailGeneratorController.java
+++ src/main/java/com/email/writer/controllers/EmailGeneratorController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
-@AllArgsConstructor
 @CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
--- src/main/java/com/email/writer/model/EmailRequest.java
+++ src/main/java/com/email/writer/model/EmailRequest.java
@@ -3,8 +3,10 @@ package com.email.writer.model;
 
 import lombok.Data;
 
+
 @Data
 public class EmailRequest {
     private String emailContent;
     private String tone;
+
 }
